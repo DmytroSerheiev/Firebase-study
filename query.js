b986348fca928f3ea7a6a67c53a11980d151963c
Dmytro <DSerSmart@gmail.com>
2023-10-12 21:59:59 +0300

    Query.js

--- query.js
+++ query.js
@@ -1,32 +1,48 @@
-import { db } from './config';
+import { db } from "./config";
 
 // Single Doc Read
-const ref = db.collection('posts').doc('postId')
+const ref = db.collection("posts").doc("postId");
 
 // Subcollection Read
-const ref = db.collection('posts').doc('postId').collection('tags');
-
+const ref = db.collection("posts").doc("postId").collection("tags");
 
 // Bucket Read
 
-const post = db.collection('posts').doc('postId')
-const tags = db.collection('tags').doc('postId')
-
+const post = db.collection("posts").doc("postId");
+const tags = db.collection("tags").doc("postId");
 
 // Multi-document read
 
-const post = await db.collection('posts').doc('postId').get();
+const post = await db.collection("posts").doc("postId").get();
 
 const tagIds = post.data().tags;
 
-const tagReads = tagIds.map(tag => db.collection('tags').get(tag));
+const tagReads = tagIds.map((tag) => db.collection("tags").get(tag));
 
 const tags = await Promise.all(tagReads);
 
-
 // Helper: Reads an array of IDs from a collection concurrently
 const readIds = async (collection, ids) => {
-    const reads = ids.map(id => collection.doc(id).get() );
-    const result = await Promise.all(reads);
-    return result.map(v => v.data());
-}
+  const reads = ids.map((id) => collection.doc(id).get());
+  const result = await Promise.all(reads);
+  return result.map((v) => v.data());
+
+  //========================================
+
+  // Basic Where
+  const rangeQuery = db.collection("users").where("age", ">=", 21);
+
+  // AND
+  const andQuery = db
+    .collection("users")
+    .where("age", "==", 21)
+    .where("sex", "==", "M");
+
+  // OR
+  const q1 = db.collection("users").where("age", "==", 21);
+  const q2 = db.collection("users").where("age", "==", 25);
+
+  // NOT
+  const not1 = db.collection("users").where("age", ">", 21);
+  const not2 = db.collection("users").where("age", "<", 21);
+};
